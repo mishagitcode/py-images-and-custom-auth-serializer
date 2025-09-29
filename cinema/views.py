d0d5699dda7707a571f707a4441ac8b32217242d
Misha Zubritsky <zubritsky.misha@gmail.com>
2025-09-29 21:06:07 +0300

    implemented v3

--- cinema/views.py
+++ cinema/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
@@ -112,6 +113,7 @@ class MovieViewSet(
         methods=["POST"],
         detail=True,
         permission_classes=[IsAdminUser],
+        parser_classes=[MultiPartParser],
         url_path="upload-image"
     )
     def upload_image(self, request, pk=None):
